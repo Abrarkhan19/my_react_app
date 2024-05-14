5510048d220f12e0b85dbcba1396838115604e61
Abrar Khan <abrarkhan12911@gmail.com>
2024-05-14 16:43:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deployed by Abrar Khan
+          Deployed by Abrar
         </a>
       </header>
     </div>
